26fc7ad8bc4563a852bc001ceeac155b4021ff82
islambahaaa <bossislam55@gmail.com>
2024-05-20 03:02:01 +0300

    app_recording

--- lib/widgets/news_view.dart
+++ lib/widgets/news_view.dart
@@ -3,7 +3,7 @@ import 'package:news_app/models/article_model.dart';
 import 'package:news_app/views/article_view.dart';
 
 class NewsView extends StatelessWidget {
-  NewsView({super.key, required this.articleModel});
+  const NewsView({super.key, required this.articleModel});
   final ArticleModel articleModel;
   @override
   Widget build(BuildContext context) {
@@ -21,13 +21,15 @@ class NewsView extends StatelessWidget {
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(6),
-            child: Image.network(
-              articleModel.image ??
-                  'https://www.pacificfoodmachinery.com.au/media/catalog/product/placeholder/default/no-product-image-400x400_1.png',
-              height: 200,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
+            child: articleModel.image != null
+                ? Image.network(
+                    articleModel.image!,
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  )
+                : Image.asset('assets/images/NoImage.png',
+                    height: 200, width: double.infinity, fit: BoxFit.cover),
           ),
           const SizedBox(
             height: 12,
@@ -36,7 +38,7 @@ class NewsView extends StatelessWidget {
             articleModel.title,
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.black,
               fontSize: 20,
               fontWeight: FontWeight.w500,
@@ -49,7 +51,7 @@ class NewsView extends StatelessWidget {
             articleModel.subTitle ?? '',
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.grey,
               fontSize: 14,
             ),
